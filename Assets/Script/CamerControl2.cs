6e40da3c4c666907061f8b6934f8e07024fc8af7
两口布丁 <halole40@gmail.com>
2020-02-22 16:12:18 +0800

    Spirit V0.1

---
+++ Assets/Script/CamerControl2.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerControl2 : MonoBehaviour {
+
+    public GameObject rotateY;
+    public GameObject rotateX;
+
+
+    private Quaternion targetTransY;
+    private Quaternion targetTransX;
+
+
+    private float mouseY;
+    private float mouseX;
+
+
+    private float YAngleChange;
+    private float XAngleChange;
+
+
+    private float smoothSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void LateUpdate()
+    {
+        mouseX = Input.GetAxis("Mouse X");
+        mouseY = Input.GetAxis("Mouse Y");
+
+
+        RotateByX(mouseY);
+        RotateByY(mouseX);
+    }
+
+    void RotateByY(float xValue)
+    {
+        YAngleChange += xValue * smoothSpeed * Time.deltaTime * 90;
+        targetTransY = Quaternion.Euler(0, YAngleChange, 0);
+        rotateY.transform.localRotation = Quaternion.Slerp(rotateY.transform.localRotation, targetTransY, 2);
+
+
+    }
+    void RotateByX(float yValue)
+    {
+        XAngleChange -= yValue * smoothSpeed * Time.deltaTime * 90;
+        XAngleChange = Mathf.Clamp(XAngleChange, -70f, 40f);
+        targetTransX = Quaternion.Euler(XAngleChange, 0, 0);
+        rotateX.transform.localRotation = Quaternion.Slerp(rotateX.transform.localRotation, targetTransX, 2);
+    }
+}
---
+++ Assets/Script/MainCharactor.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainCharactor : MonoBehaviour {
+
+    public float _moveSpeed;
+
+    private Transform _chaTransform;
+    private Rigidbody _chaRigidbody;
+
+	// Use this for initialization
+	void Start () {
+        _chaRigidbody = gameObject.GetComponent<Rigidbody>();
+        _chaTransform = gameObject.GetComponent<Transform>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+        //角色移动WASD，空格闪避
+        //if (Input.GetKey(KeyCode.W))
+        //{
+        //    transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime);
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        transform.Translate(Vector3.forward * _moveSpeed * Time.deltaTime * 5);
+        //    }
+        //}
+        //if (Input.GetKey(KeyCode.S))
+        //{
+        //    transform.Translate(Vector3.back * _moveSpeed * Time.deltaTime);
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        transform.Translate(Vector3.back * _moveSpeed * Time.deltaTime * 5);
+        //    }
+        //}
+        //if (Input.GetKey(KeyCode.A))
+        //{
+        //    transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime);
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        transform.Translate(Vector3.left * _moveSpeed * Time.deltaTime * 5);
+        //    }
+        //}
+        //if (Input.GetKey(KeyCode.D))
+        //{
+        //    transform.Translate(Vector3.right * _moveSpeed * Time.deltaTime);
+        //    if (Input.GetKeyDown(KeyCode.Space))
+        //    {
+        //        transform.Translate(Vector3.right * _moveSpeed * Time.deltaTime * 5);
+        //    }
+        //}
+    }
+
+    private void LateUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+        float v = Input.GetAxis("Vertical");
+        Vector3 vector3 = new Vector3(h, 0, v);
+        _chaRigidbody.MovePosition(_chaTransform.position + vector3 * _moveSpeed * Time.deltaTime);
+    }
+}
